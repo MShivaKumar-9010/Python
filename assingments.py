a5abad35f31f3f7c568d19105d3009c8188b76f4
MShivaKumar-9010 <makalashivakumar206@gmail.com>
2026-08-05 22:24:36 +0530

    Add files via upload

---
+++ assingments.py
@@ -0,0 +1,47 @@
+#Store the value 500 in a variable called balance.
+balance=500
+print(balance)
+
+
+#A wallet initially has ₹800. Add ₹250 to it using a shortcut assignment.
+
+wallet=800
+wallet+=250
+print(wallet)
+
+
+#A player has 100 health points. Reduce the health by 35 using a shortcut assignment
+points=100
+points-=35
+print(points)
+
+
+#A salary is ₹40,000. Increase it by multiplying it by 2 using a shortcut assignment
+salary=40000
+salary*=2
+print(salary)
+
+
+#A total distance is 600 km. Divide it equally among 3 drivers using a shortcut assignment.
+distance=600
+distance/=3
+print(distance)
+
+
+#A number is 96. Update it by dividing it into groups of 5 and keeping only the complete groups.
+number=96
+number//=5
+print(number)
+
+
+#Store the value 8 in a variable and update it by raising it to the power of 3 using a shortcut assignment.
+value=8
+value=8**3
+print(value)
+
+
+#A warehouse has 125 items. After packing them into groups of 9, update the variable to store only the remaining items.
+items=125
+groups=9
+remaining_items=items%groups
+print(remaining_items)
