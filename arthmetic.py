87399bc61fac871ccedb1176bc9290c58ae35176
MShivaKumar-9010 <makalashivakumar206@gmail.com>
2026-08-05 22:23:30 +0530

    Add files via upload

---
+++ arthmetic.py
@@ -0,0 +1,74 @@
+
+#A product costs ₹450 and a customer buys 6 of them. Calculate the total bill.
+cost = 450
+quantity = 6
+
+total_bill = cost * quantity
+print("total_bill:$",total_bill)
+
+
+#A student scored 480 marks out of 600. Calculate the percentage.
+marks=480
+totalmarks = 600
+
+percentage = (marks/totalmarks)*100
+print("percentage:%",percentage)
+
+#A company has 125 employees. Divide them equally into 8 teams. Find how many employees are in each team.
+employees = 125
+divide = 8
+
+team = employees//divide
+print(team)
+remaining=employees%divide
+print(remaining)
+
+
+#A bakery made 95 cupcakes and packs them into boxes of 12. Find how many completely filled boxes can be made.
+cupcakes=95
+boxes=12
+
+totalfilled_boxes=cupcakes//boxes
+print(totalfilled_boxes)
+
+
+#After packing the cupcakes into boxes of 12, find how many cupcakes are left.
+cupcakes=95
+boxes=12
+
+remaining_cupcakes=cupcakes%boxes
+print(remaining_cupcakes)
+
+#A square has a side length of 14 cm. Find its area.
+side=14
+area=side*side
+print(area)
+
+
+#A number is multiplied by itself 5 times. Write a program for the calculation
+number=5
+multiplies=5**5
+print(multiplies)
+
+
+#A person earns ₹1,250 per day for 28 days. Find the total earnings.
+perday=1250
+
+total_earnings=perday*28
+print(total_earnings)
+
+
+#Rahul has ₹2,500 and spends ₹1,375. Find the remaining balance.
+totalamount=2500
+spent=1375
+
+remaining_amount=totalamount-spent
+print(remaining_amount)
+
+
+#Find the average of three numbers: 45, 78, and 92
+a=45
+b=78
+c=92
+avg=(a+b+c)/3
+print(avg)
